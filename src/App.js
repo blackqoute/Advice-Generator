daa4bcb977d5fd2e08b42b00aadef548b6c41d06
blackqoute <harishgonzales10@gmail.com>
2022-10-03 20:17:50 +0800

    Advice Generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
 
 function App() {
+
+    let [advice, setAdvice] = useState('');
+    let [id, setId] = useState(0);
+
+    const url = 'https://api.adviceslip.com/advice';
+    
+
+    useEffect(() => {
+     const fetchData = async () => {
+      try{
+        const res = await fetch(url);
+        const data = await res.json();
+        setAdvice(data.slip.advice)
+        setId(data.slip.id)
+        console.log(data)
+      }catch (error) {
+        console.log("error", error);
+		}
+     }
+  fetchData();
+
+
+    },[])
+
+    const handleOnClick = () => {
+    
+      const fetchData = async () => {
+        try{
+          const res = await fetch(url);
+          const data = await res.json();
+          setAdvice(data.slip.advice)
+          setId(data.slip.id)
+          console.log(data)
+        }catch (error) {
+          console.log("error", error);
+      }
+       }
+       fetchData();
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="card">
+           <h2>Advice # {id}</h2>
+           <h1>{advice}</h1>
+           <button onClick={handleOnClick}>New Advice</button>
+      </div>
+  
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
